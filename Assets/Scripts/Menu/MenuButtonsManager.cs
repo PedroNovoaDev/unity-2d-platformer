973d8d21129d2b119f0e54b47eed7cfe7edb7eca
Pedro Novoa <pedronovoa.21@gmail.com>
2023-04-09 02:07:53 -0300

    Added Menu Scene / Level01 Scene

---
+++ Assets/Scripts/Menu/MenuButtonsManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuButtonsManager : MonoBehaviour
+{
+    #region Variables
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+    #endregion
+
+    #region Methods
+
+    // *Menu animation explanation*
+    // The ideia is that the buttons show one after the other popping in the screen.
+    // At first we set the buttons scale to 0 and hide all of them.
+    // Then we set one by one to active with the animation of scaling to 1.
+
+    private void OnEnable()
+    {
+        HideAllButtons();
+        ShowButtons();
+    }
+
+    private void HideAllButtons()
+    {
+        foreach (var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+
+    private void ShowButtons()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i*delay).SetEase(ease);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    #region Methods
+
+    // *Load Scene explanation*
+    // The ideia is that we can reutilize the script in different ways to load a new scene.
+    // To do that we use 2 equal methods that receive different parameters.
+    // Like that we can add the script to the button that'll do the action and pass the parameters in Unity inspector.
+
+    public void Load(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void Load(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+    #endregion
+}
